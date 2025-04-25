49ed6f5436dec6dffa4e0ad464287239edcfa35c
doquanghuy <admin@doitsu.tech>
2025-04-25 10:17:04 +0700

    Testing 0.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { ActivityStateProvider } from './context/ActivityStateContext';
 import MagicPointsDebug from './components/MagicPointsDebug';
+import NotificationDisplay from './components/NotificationDisplay'; // Import NotificationDisplay
 // ...existing imports...
 
 function App() {
   return (
     <ActivityStateProvider>
       {/* Your existing app content */}
+      <NotificationDisplay /> {/* Render NotificationDisplay */}
       <MagicPointsDebug />
     </ActivityStateProvider>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MagicPointsProvider } from './context/MagicPointsContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { SocketProvider } from './context/SocketContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <MagicPointsProvider>
-        <App />
-      </MagicPointsProvider>
+      <AuthProvider>
+        <SocketProvider>
+          <MagicPointsProvider>
+            <App />
+          </MagicPointsProvider>
+        </SocketProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
